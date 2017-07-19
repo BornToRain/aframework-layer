1d28c8c6ee8406658c7f8aa2bb79d110f1bf0a95
alvis <87922072>
2017-07-19 13:12:45 +0800

    test

---
+++ aframework/aframework-service/src/test/java/service/test/CustomerTestCase.java
@@ -0,0 +1,89 @@
+package service.test;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.domain.customers.Customer;
+import com.service.customers.ICustomerService;
+
+import testbase.BaseTestCase;
+
+
+
+
+/**
+ * @ClassName CustomerTestCase
+ * @Description Customer Test Classs
+ * @author Alvis
+ * @Date Jul 7, 2017 9:37:55 PM
+ * @version 1.0.0
+ */
+public class CustomerTestCase extends BaseTestCase {
+
+    @Autowired
+    private ICustomerService customerService;
+
+  
+    @Test
+    public void InsertCustomersTest() {
+       
+        List<Customer> insertcustomers = new ArrayList<Customer>(2);
+
+        for (int i = 0; i < 2; i++) {
+            Customer custoemr = new Customer();
+            custoemr.setName("alvis" + i);
+            custoemr.setAge(10 + i);
+            custoemr.setLastActiveTime(new Timestamp(System.currentTimeMillis()));
+            insertcustomers.add(custoemr);
+        }
+        customerService.insertCustomers(insertcustomers);
+
+        for (Customer customer : insertcustomers) {
+            System.out.println(customer.getId());
+        }
+    }
+
+
+
+    @Test
+    public void CustomersPageListTest() {
+
+        List<Customer> customers = customerService.customerPageList("alvis", 0, 10);
+        for (Customer customer : customers) {
+            System.out.println(customer.getName());
+        }
+
+    }
+
+    @Test
+    public void UpdateCustomerTest() {
+        Customer customer = customerService.getCustomerById(79);
+        customer.setAge(99);
+        customerService.updateCustomer(customer);
+    }
+
+    @Test
+    public void UpdateCustomersAgeTest() {
+        List<Integer> ids = new ArrayList<Integer>();
+        ids.add(80);
+        ids.add(81);
+        ids.add(82);
+        customerService.updateCustomersAge(98, ids);
+    }
+
+    @Test
+    public void DeleteCutomerByIdsTest() {
+        List<Integer> ids = new ArrayList<Integer>();
+        ids.add(80);
+        ids.add(81);
+        ids.add(82);
+        customerService.deleteCutomerByIds(ids);
+    }
+
+}
---
+++ aframework/aframework-service/src/test/java/testbase/BaseTestCase.java
@@ -0,0 +1,20 @@
+package testbase;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+  
+/**
+ * @ClassName BaseTestCase
+ * @Description Test Case Base
+ * @author Alvis
+ * @Date Jul 7, 2017 9:38:32 PM
+ * @version 1.0.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)  //使用junit4进行测试  
+@ContextConfiguration(locations={"classpath*:/testbase/springTest-config.xml"})   
+public class BaseTestCase {
+
+}
+ 
