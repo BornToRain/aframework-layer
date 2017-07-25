e910bb96dc25d1f891330e85f1ef439a1d29c7a3
alvis <87922072>
2017-07-25 17:06:18 +0800

    update

--- aframework/aframework-web/src/main/java/com/controllers/font/CustomerController.java
+++ aframework/aframework-web/src/main/java/com/controllers/font/CustomerController.java
@@ -4,9 +4,13 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.domain.customers.Customer;
+import com.service.customers.ICustomerService;
 
 /**
  * @ClassName CustomerController
@@ -19,46 +23,53 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/customer")
 public class CustomerController extends BaseFontController {
 
-	@RequestMapping("/login")
-	public String Login() {
-		return prefView + "/customer/login";
-	}
+    @Autowired
+    private ICustomerService customerService;
 
-	@RequestMapping(value = "/loginPost", method = RequestMethod.POST)
-	public String LoginPost(String username, String password) {
+    @RequestMapping("/login")
+    public String Login() {
+        return prefView + "/customer/login";
+    }
 
-		Subject currentUser = SecurityUtils.getSubject();
-		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-		try {
-			currentUser.login(token);
-			// User user = userService.findByUsername(username);
-			// subject.getSession().setAttribute("user", user);
+    @RequestMapping(value = "/loginPost", method = RequestMethod.POST)
+    public String LoginPost(String username, String password) {
+        Subject currentUser = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
 
-			System.out.println("登录成功");
-			return "redirect:/admin/customer/index";
-			// return "redirect:/Home/Index";
+            currentUser.login(token);
+            Customer customer = customerService.getCustomerByUserName(username);
+            currentUser.getSession().setAttribute("customer", customer);
+            return "redirect:/admin/customer/index";
+            // return "redirect:/Home/Index";
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            return prefView + "/customer/login";
+        }
 
-		} catch (AuthenticationException e) {
-			e.printStackTrace();
-			System.out.println("登录失败");
-			return prefView + "/customer/login";
+        // 是否认证通过
+        // boolean isAuthenticated = currentUser.isAuthenticated();
+        // System.out.println("是否认证通过: " + isAuthenticated);
 
-		}
+        // 退出操作
+        // currentUser.logout();
 
-		// 是否认证通过
-		// boolean isAuthenticated = currentUser.isAuthenticated();
-		// System.out.println("是否认证通过: " + isAuthenticated);
+        // isAuthenticated = currentUser.isAuthenticated();
+        // System.out.println("是否认证通过: " + isAuthenticated);
 
-		// 退出操作
-		// currentUser.logout();
+        // 基于资源的授权(权限标识符)
+        // boolean permitted = currentUser.isPermitted("user:create");
+        // System.out.println("基于资源的授权: " + permitted);
 
-		// isAuthenticated = currentUser.isAuthenticated();
-		// System.out.println("是否认证通过: " + isAuthenticated);
-
-		// 基于资源的授权(权限标识符)
-		// boolean permitted = currentUser.isPermitted("user:create");
-		// System.out.println("基于资源的授权: " + permitted);
-
-	}
+    }
+    
+    
+    
+    @RequestMapping("/loginout")
+    public String Loginout() {
+        Subject currentUser = SecurityUtils.getSubject();
+        currentUser.logout();
+        return prefView + "/customer/login";
+    }
 
 }
