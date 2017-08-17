2b54eabc382d1acd40c8f840b72ca57f145475f5
alvis <87922072>
2017-08-17 16:39:48 +0800

    db

--- DataBase/AFData.sql
+++ DataBase/AFData.sql
@@ -1,41 +1,42 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 172.16.2.86
+Source Server         : 172.16.2.85
 Source Server Version : 50636
-Source Host           : 172.16.2.86:3306
+Source Host           : 172.16.2.85:3306
 Source Database       : AFData
 
 Target Server Type    : MYSQL
 Target Server Version : 50636
 File Encoding         : 65001
 
-Date: 2017-08-16 14:06:22
+Date: 2017-08-17 16:30:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for role
+-- Table structure for t_role
 -- ----------------------------
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `system_code` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of role
+-- Records of t_role
 -- ----------------------------
-INSERT INTO `role` VALUES ('9', '管理员', 'admin');
+INSERT INTO `t_role` VALUES ('9', '管理员', 'admin');
+INSERT INTO `t_role` VALUES ('11', '管理员', 'admin');
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for t_user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_uuid` char(36) DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
@@ -44,22 +45,22 @@ CREATE TABLE `user` (
   `age` int(11) DEFAULT NULL,
   `last_active_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=198 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user
+-- Records of t_user
 -- ----------------------------
-INSERT INTO `user` VALUES ('172', '05d44c79-627b-466c-940a-c62074107226', 'alvis', 'TRMYMCbWUzLbyLjQu11nkUA8adT+sAyIxRIiLEPLnjFkMch+omlYC1TzVjW3E7eDFR2EdMc2NMpVrZT9GVKGQPl4lzH6/6MIG70k+asZNiDdWh7ZGPqIGGkGbJa/5h/emNnGwGIhRBKnJ47kiIhgPlS86XTVBPLufHYn4jwxSzk=', 'alvis', null, '2017-08-10 11:06:47');
-INSERT INTO `user` VALUES ('185', '100d479d-4a56-4117-8bee-82b12887a4df', 'alvis1', 'qVYBhBlAbUBYXeKxKhR1/1Sl3GPeWanoEPackz1bPPOnQj1iPmVWaRfKqWJ/yAd9yR4VpAyWpWXTWQj21bEi2usS/6Oi1hSrvw/KunkaSEUw01+LyJUordP8oiyVpipfR5099oZfvzbBRCUJFed6oaX7DQ/zejIIjNk+WnfTLK4=', 'alvis1', null, '2017-08-10 17:33:45');
-INSERT INTO `user` VALUES ('186', null, null, null, 'alvis0', '10', '2017-08-11 12:33:57');
-INSERT INTO `user` VALUES ('187', null, null, null, 'alvis1', '11', '2017-08-11 12:33:57');
-INSERT INTO `user` VALUES ('188', null, null, null, 'alvis0', '10', '2017-08-11 12:33:57');
-INSERT INTO `user` VALUES ('189', null, null, null, 'alvis0', '10', '2017-08-11 12:34:18');
-INSERT INTO `user` VALUES ('190', null, null, null, 'alvis1', '11', '2017-08-11 12:34:18');
-INSERT INTO `user` VALUES ('191', null, null, null, 'alvis0', '10', '2017-08-11 12:34:18');
-INSERT INTO `user` VALUES ('192', null, null, null, 'alvis0', '10', '2017-08-11 12:34:52');
-INSERT INTO `user` VALUES ('193', null, null, null, 'alvis1', '11', '2017-08-11 12:34:52');
-INSERT INTO `user` VALUES ('194', null, null, null, 'alvis0', '10', '2017-08-11 12:34:52');
-INSERT INTO `user` VALUES ('195', 'a70f14c1-a003-494a-b2a8-d78c6a6cd312', null, null, 'alvis0', '10', '2017-08-14 16:30:13');
-INSERT INTO `user` VALUES ('196', '7e58ebcc-9c1e-4d4c-9e39-f8e98840078c', null, null, 'alvis1', '11', '2017-08-14 16:30:13');
-INSERT INTO `user` VALUES ('197', '78c8a12a-ca6c-46b4-a37b-56a574ed701e', 'test', 'YTmFukL78ryT0yQclt7cepRer+/6oaN3qY6Clcemkc6C4tTXVm4vBuwed3faDYLhEauVxJt+7dJmSSo402dxiOwJ8H0HBEF6BQo6lubDX8KcudPTvwOmkX7G6Hsd+z9UNvSU0Ju5FioN9LMPLMwbC72S4ja52tiA0K4o68oROnQ=', 'test', null, '2017-08-14 16:40:21');
+INSERT INTO `t_user` VALUES ('172', '05d44c79-627b-466c-940a-c62074107226', 'alvis', 'TRMYMCbWUzLbyLjQu11nkUA8adT+sAyIxRIiLEPLnjFkMch+omlYC1TzVjW3E7eDFR2EdMc2NMpVrZT9GVKGQPl4lzH6/6MIG70k+asZNiDdWh7ZGPqIGGkGbJa/5h/emNnGwGIhRBKnJ47kiIhgPlS86XTVBPLufHYn4jwxSzk=', 'alvis', null, '2017-08-10 11:06:47');
+INSERT INTO `t_user` VALUES ('185', '100d479d-4a56-4117-8bee-82b12887a4df', 'alvis1', 'qVYBhBlAbUBYXeKxKhR1/1Sl3GPeWanoEPackz1bPPOnQj1iPmVWaRfKqWJ/yAd9yR4VpAyWpWXTWQj21bEi2usS/6Oi1hSrvw/KunkaSEUw01+LyJUordP8oiyVpipfR5099oZfvzbBRCUJFed6oaX7DQ/zejIIjNk+WnfTLK4=', 'alvis1', null, '2017-08-10 17:33:45');
+INSERT INTO `t_user` VALUES ('186', null, null, null, 'alvis0', '10', '2017-08-11 12:33:57');
+INSERT INTO `t_user` VALUES ('187', null, null, null, 'alvis1', '11', '2017-08-11 12:33:57');
+INSERT INTO `t_user` VALUES ('188', null, null, null, 'alvis0', '10', '2017-08-11 12:33:57');
+INSERT INTO `t_user` VALUES ('189', null, null, null, 'alvis0', '10', '2017-08-11 12:34:18');
+INSERT INTO `t_user` VALUES ('190', null, null, null, 'alvis1', '11', '2017-08-11 12:34:18');
+INSERT INTO `t_user` VALUES ('191', null, null, null, 'alvis0', '10', '2017-08-11 12:34:18');
+INSERT INTO `t_user` VALUES ('192', null, null, null, 'alvis0', '10', '2017-08-11 12:34:52');
+INSERT INTO `t_user` VALUES ('193', null, null, null, 'alvis1', '11', '2017-08-11 12:34:52');
+INSERT INTO `t_user` VALUES ('194', null, null, null, 'alvis0', '10', '2017-08-11 12:34:52');
+INSERT INTO `t_user` VALUES ('195', 'a70f14c1-a003-494a-b2a8-d78c6a6cd312', null, null, 'alvis0', '10', '2017-08-14 16:30:13');
+INSERT INTO `t_user` VALUES ('196', '7e58ebcc-9c1e-4d4c-9e39-f8e98840078c', null, null, 'alvis1', '11', '2017-08-14 16:30:13');
+INSERT INTO `t_user` VALUES ('197', '78c8a12a-ca6c-46b4-a37b-56a574ed701e', 'test', 'YTmFukL78ryT0yQclt7cepRer+/6oaN3qY6Clcemkc6C4tTXVm4vBuwed3faDYLhEauVxJt+7dJmSSo402dxiOwJ8H0HBEF6BQo6lubDX8KcudPTvwOmkX7G6Hsd+z9UNvSU0Ju5FioN9LMPLMwbC72S4ja52tiA0K4o68oROnQ=', 'test', null, '2017-08-14 16:40:21');
--- DataBase/AFOrder.sql
+++ DataBase/AFOrder.sql
@@ -1,37 +1,34 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 172.16.2.86
+Source Server         : 172.16.2.85
 Source Server Version : 50636
-Source Host           : 172.16.2.86:3306
+Source Host           : 172.16.2.85:3306
 Source Database       : AFOrder
 
 Target Server Type    : MYSQL
 Target Server Version : 50636
 File Encoding         : 65001
 
-Date: 2017-08-16 14:05:17
+Date: 2017-08-17 16:30:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for order
+-- Table structure for t_order
 -- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uuid` char(36) NOT NULL,
   `user_id` int(11) NOT NULL,
   `unit_price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of order
+-- Records of t_order
 -- ----------------------------
-INSERT INTO `order` VALUES ('1', 'a8b8f37b-3026-4e15-9263-4ff52957b42f', '1', '1.20');
-INSERT INTO `order` VALUES ('2', 'a16e6e25-eaa9-49c4-bcef-065610711c16', '1', '1.20');
-INSERT INTO `order` VALUES ('3', '5d180474-1c3c-434e-8630-df1a0507ecfc', '1', '1.20');
-INSERT INTO `order` VALUES ('4', '603d8b51-4406-4780-997a-8ff22f9a2030', '1', '1.20');
-INSERT INTO `order` VALUES ('5', 'a053a605-edb5-4c68-8bb5-3e731be86518', '1', '1.30');
+INSERT INTO `t_order` VALUES ('11', '268e45e6-a656-4de4-91e2-e0f8f046daac', '1', '1.30');
+INSERT INTO `t_order` VALUES ('12', '1e96ff9f-98f8-4c20-9e0a-8964f9a31ee6', '1', '1.30');
