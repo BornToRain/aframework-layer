aa3ac375aa7076c0d580c621b0210a866631b78f
alvis <87922072>
2018-03-15 19:11:42 +0800

    update

--- aframework/aframework-web/src/main/java/com/web/controllers/HomeController.java
+++ aframework/aframework-web/src/main/java/com/web/controllers/HomeController.java
@@ -5,7 +5,10 @@ import aframework.configure.authentication.AfAuthUser;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 /**
  * @author Alvis
@@ -15,11 +18,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Date Jul 6, 2017 4:12:46 PM
  */
 @Controller("FHomeController")
-@RequestMapping("/home")
 public class HomeController extends BaseWebController {
 
-    @RequestMapping("/index")
-    public String Index() {
+    @GetMapping("/")
+    String Index() {
+        return "redirect:/home/index";
+    }
+
+    @RequestMapping("/home/index")
+    public String HomeIndex() {
         return prefView + "/home/index";
     }
 
