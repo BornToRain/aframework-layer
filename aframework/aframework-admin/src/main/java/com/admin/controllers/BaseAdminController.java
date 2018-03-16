e37d64ecc97663afafa9e32052173dd5db39f12d
alvis <87922072>
2018-03-16 14:19:41 +0800

    add js version contrller

--- aframework/aframework-admin/src/main/java/com/admin/controllers/BaseAdminController.java
+++ aframework/aframework-admin/src/main/java/com/admin/controllers/BaseAdminController.java
@@ -10,7 +10,10 @@ import aframework.configure.mvc.BaseController;
  * @Date Jul 6, 2017 4:12:16 PM
  */
 public class BaseAdminController extends BaseController {
-
+    /**
+     * aframework-admin.version
+     */
+    public static final String VERSION = "1.0.1";
     final static String prefView = "admin";
     final static Integer pageSize = 10;
 }
--- aframework/aframework-web/src/main/java/com/web/controllers/BaseWebController.java
+++ aframework/aframework-web/src/main/java/com/web/controllers/BaseWebController.java
@@ -3,13 +3,18 @@ package com.web.controllers;
 import aframework.configure.mvc.BaseController;
 
 /**
+ * @author Alvis
+ * @version 1.0.0
  * @ClassName BaseWebController
  * @Description BaseWebController
- * @author Alvis
  * @Date Jul 6, 2017 4:12:38 PM
- * @version 1.0.0
  */
 public class BaseWebController extends BaseController {
+
+    /**
+     * aframework-web.version
+     */
+    public static final String VERSION = "1.0.1";
 
     final static String prefView = "web";
     final static Integer pageSize = 10;
