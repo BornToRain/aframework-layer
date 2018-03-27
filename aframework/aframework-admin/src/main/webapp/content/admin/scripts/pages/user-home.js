65e4ad6401caf2265e7e1c791134a12654d36947
alvis <87922072>
2018-03-27 11:43:49 +0800

    page bug

--- aframework/aframework-admin/src/main/webapp/content/admin/scripts/pages/user-home.js
+++ aframework/aframework-admin/src/main/webapp/content/admin/scripts/pages/user-home.js
@@ -1,64 +1,65 @@
-var UserHome = function() {
+var UserHome = function () {
 
-	function talbeRender(data) {
-		var renderitems = "";
-		var itemrendermodel = '<tr>\
+    function talbeRender(data) {
+        var renderitems = "";
+        var itemrendermodel = '<tr>\
 								<th>{id}</th>\
 								<th>{name}</th>\
 								<th>{age}</th>\
 								<th>{lastActiveTime}</th>\
 							   </tr>';
-		data.itemList.forEach(function(val) {
-			var indexel = itemrendermodel;
-			for ( var name in val) {
-				if (val.hasOwnProperty(name)) {
-					indexel = indexel.replace("{" + name + "}", val[name]);
-				}
-			}
-			renderitems += indexel;
-		});
-		$("#tableContain").html(renderitems);
-	}
+        data.itemList.forEach(function (val) {
+            var indexel = itemrendermodel;
+            for (var name in val) {
+                if (val.hasOwnProperty(name)) {
+                    indexel = indexel.replace("{" + name + "}", val[name]);
+                }
+            }
+            renderitems += indexel;
+        });
+        $("#tableContain").html(renderitems);
+    }
 
-	function dataPost(postdata) {
-		$.post(ctx + "/admin/user/page", postdata, function(response) {
-			talbeRender(response);
-			if (response.pageCount != 0) {
-				var options = {
-					numberOfPages : response.pageSize,
-					currentPage : response.pageIndex,
-					totalPages : response.pageCount,
-					onPageChanged : function(e, oldPage, newPage) {
-						var post = $("#searchForm").serializeArray();
-						post.push({"name":"pageIndex","value":newPage}) 
-						dataPost(post);
-					}
-				}
-				$("#pageItems").bootstrapPaginator(options);
-			} else {
-				$("#pageItems").empty();
-			}
-		});
-	}
+    function dataPost(postdata) {
+        $.post(ctx + "/admin/user/page", postdata, function (response) {
+            talbeRender(response);
+            if (response.pageCount != 0) {
+                var options = {
+                    numberOfPages: response.pageSize,
+                    currentPage: response.pageIndex,
+                    totalPages: response.pageCount,
+                    onPageChanged: function (e, oldPage, newPage) {
+                        var post = $("#searchForm").serializeArray();
+                        post.push({"name": "pageIndex", "value": newPage})
+                        dataPost(post);
+                    }
+                }
+                $("#pageItems").bootstrapPaginator(options);
+            } else {
+                $("#pageItems").empty();
+            }
+        });
+    }
 
-	function tableInint() {
+    function tableInint() {
 
-		dataPost({
-			pageIndex : 1
-		});
-	}
+        dataPost({
+            pageIndex: 1
+        });
+    }
 
-	function bind() {
-		$("#search").click(function() {
-			var post = $("#searchForm").serialize();
-			dataPost(post);
-		});
-	}
+    function bind() {
+        $("#search").click(function () {
+            var post = $("#searchForm").serialize();
+            post += "&pageIndex=1";
+            dataPost(post);
+        });
+    }
 
-	return {
-		init : function() {
-			tableInint();
-			bind();
-		}
-	};
+    return {
+        init: function () {
+            tableInint();
+            bind();
+        }
+    };
 }();
