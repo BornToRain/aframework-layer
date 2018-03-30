62ec42f05a8ea5b44fb56d3388d2584d56bc2e77
alvis <87922072>
2018-03-30 14:13:24 +0800

    explain

--- aframework/aframework-core/src/test/java/core/test/SnowFlakeTest.java
+++ aframework/aframework-core/src/test/java/core/test/SnowFlakeTest.java
@@ -24,6 +24,7 @@ public class SnowFlakeTest {
 
     @Test
     public void SequenceTest() {
+        // 0 - 41位时间戳 - 5位数据中心标识 - 5位机器标识 - 12位序列号
         SnowFlake snowFlake = new SnowFlake(2, 3);
         for (int i = 0; i < (1 << 12); i++) {
             System.out.println(snowFlake.nextId());
